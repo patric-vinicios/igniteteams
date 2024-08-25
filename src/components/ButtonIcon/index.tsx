cc192ebe8b645a70108dbd1f11a2db602d540f55
Patric Vinicios <psdev7@gmail.com>
2024-08-25 20:29:49 -0300

    feat: second commit

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,16 +1,18 @@
 import { TouchableOpacityProps } from 'react-native'
+import {MaterialIcons} from '@expo/vector-icons'
 import * as S from './styles'
 
 type Props = TouchableOpacityProps & {
-    
+    icon: keyof typeof MaterialIcons.glyphMap;
+    type?: S.ButtonIconTypeStyleProps
 }
 
-export function ButtonIcon({} : Props) {
+export function ButtonIcon({icon, type = 'PRIMARY', ...rest} : Props) {
     return (
-        <S.Container>
+        <S.Container {...rest}>
             <S.Icon
-                name="home"
-                type="SECONDARY"
+                name={icon}
+                type={type}
             />
         </S.Container>
     )
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,9 +1,20 @@
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 import { ButtonIcon } from '@components/ButtonIcon'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
+import { Input } from '@components/Input'
+import { Filter } from '@components/Filter'
+import { PlayerCard } from '@components/PlayerCard'
 import * as S from './styles'
+import { ListEmpty } from '@components/ListEmpty'
+import { Button } from '@components/Button'
 
 export function Players() {
+    const [team, setTeam] = useState('Time A')
+    const [players, setPlayers] = useState([])
+
+
     return (
         <S.Container>
             <Header showBackButton />
@@ -12,7 +23,57 @@ export function Players() {
                 title="Nome da turma"
                 subtitle="Adicione a galera e separe os times"
             />
-            <ButtonIcon />
+
+            <S.Form>
+                <Input
+                    placeholder="Nome da pessoa"
+                    autoCorrect={false}
+                />
+
+                <ButtonIcon icon="add" />
+            </S.Form>
+
+            <S.HeaderList>
+                <FlatList
+                    data={['Time A', 'Time B']}
+                    keyExtractor={item => item}
+                    renderItem={({item}) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+                    horizontal
+                />
+                <S.NumberOfPlayers>
+                    {players.length}
+                </S.NumberOfPlayers>
+            </S.HeaderList>
+
+            <FlatList
+                data={players}
+                keyExtractor={item => item}
+                renderItem={({item}) => (
+                    <PlayerCard name={item} onRemove={() => {}} />
+                )}
+                ListEmptyComponent={() => (
+                    <ListEmpty
+                        message="Não há pessoas nesse time."  
+                    />
+                )}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={[
+                    {paddingBottom: 100},
+                    players.length === 0 && {flex: 1}
+                ]}
+            />
+
+
+            <Button
+                title="Remover turma"
+                type="SECONDARY"
+            />
         </S.Container>
     )
 }
