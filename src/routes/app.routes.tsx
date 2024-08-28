e4c7b7c2dc0be661b379880cc2b62c38aeb3e450
Patric Vinicios <psdev7@gmail.com>
2024-08-27 21:37:34 -0300

    feat: define routes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Groups } from "@screens/Groups";
+import { Players } from "@screens/Players";
+import { NewGroup } from "@screens/NewGroup";
+
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+
+export function AppRoutes() {
+    return (
+        <Navigator>
+            <Screen name="Groups" component={Groups} />
+            <Screen name="Players" component={Players} />
+            <Screen name="NewGroup" component={NewGroup} />
+        </Navigator>
+    )
+}
