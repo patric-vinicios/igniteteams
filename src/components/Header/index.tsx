4bf59981fb243c86281e4491026fdbc8bbecd5ac
Patric Vinicios <psdev7@gmail.com>
2024-09-08 19:02:35 -0300

    feat: use header button to return to home screen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
 
 import logoImg from '@assets/logo.png'
 import * as S from './styles'
+import { useNavigation } from '@react-navigation/native'
 
 type Props = {
     showBackButton?: boolean
 }
 
 export function Header({showBackButton = false}: Props) {
+    const navigation = useNavigation()
+
+    function handleGoHome() {
+        navigation.navigate('groups')
+    }
+
     return (
         <S.Container>
             {showBackButton && 
-                <S.BackButton>
+                <S.BackButton onPress={handleGoHome}>
                     <S.BackIcon />
                 </S.BackButton>
             }
