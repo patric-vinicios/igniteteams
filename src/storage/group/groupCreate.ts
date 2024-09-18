f5e74f8fccac9d00959bad3825082d20924323c0
Patric Vinicios <psdev7@gmail.com>
2024-09-17 22:47:24 -0300

    feat: add exception when group exists

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,10 +1,15 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { groupsGetAll } from './groupsGetAll';
 import { GROUP_COLLECTION } from "@storage/storageConfig";
+import { AppError } from "@utils/appError";
 
 export async function groupCreate(newGroup: string) {
     try {
         const storedGroups = await groupsGetAll()
+
+        const groupAlreadyExists = storedGroups.includes(newGroup)
+
+        if (groupAlreadyExists) throw new AppError("Grupo já existente")
 
         const storage = JSON.stringify([...storedGroups, newGroup])
 
